f950020e01c68d72bed57b6904fed0a31416d9b7
Neithuz-dev <neithalprince@gmail.com>
2026-05-29 17:20:23 +0530

    new

---
+++ flowcontrol/ages.py
@@ -0,0 +1,9 @@
+age = int(input("enter your age: "))
+if age>=18:
+    print("You can vote")
+elif age == 17:
+    print("you can learn to drive")
+elif age == 16:
+    print("You can buy a lottery ticket")
+else:
+    print("You can go Trick-17or-Treating")
