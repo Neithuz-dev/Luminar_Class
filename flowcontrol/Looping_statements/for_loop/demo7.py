5d8e03d4855e5414110dcfc0ece29f186d305166
Neithuz-dev <neithalprince@gmail.com>
2026-06-09 15:27:45 +0530

    New

---
+++ flowcontrol/Looping_statements/for_loop/demo7.py
@@ -0,0 +1,4 @@
+lower = int(input("enter the lower: "))
+upper = int(input("upper limit: "))
+for i in range (lower,upper+1):
+    print(i)
---
+++ flowcontrol/Looping_statements/for_loop/fabanocci.py
@@ -0,0 +1,9 @@
+n = int(input("Enter the number: "))#7
+num1 = 0
+num2 = 1
+for i in range(n):#8 7
+    print(num1)#0 1
+    sum1 = num1+num2#1 2
+    num1 =num2#1 1
+    num2 = sum1#1 2
+#0 1
