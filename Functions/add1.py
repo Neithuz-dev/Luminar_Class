0ccfef51c90a63fdbbe74c088f7403825e463ce8
Neithuz-dev <neithalprince@gmail.com>
2026-06-15 20:20:33 +0530

    New

---
+++ Functions/add1.py
@@ -0,0 +1,9 @@
+#function without arguments and without return type
+
+def add():
+    n1= 5
+    n2 = 4
+    total = n1+n2
+    print(total)
+
+add()
---
+++ Functions/sum_n.py
@@ -0,0 +1,8 @@
+def sum_n(n):
+    total_sum = 0
+    for i in range(1,n+1):
+        total_sum+=i
+    return total_sum
+result = sum_n(5)
+print(result)
+        
