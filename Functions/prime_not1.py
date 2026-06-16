19b373b7a89a5c4ce46a53531735be503c0064e0
Neithuz-dev <neithalprince@gmail.com>
2026-06-17 00:26:43 +0530

    new

--- Functions/prime_not1.py
+++ Functions/prime_not1.py
@@ -10,31 +10,18 @@
 # # prime(2,10)
 # #
 #
-# def prime(lower,upper):
-#     result = ""
-#     for num in range(lower,upper):
-#         if num <2:
-#             result+=  f"Not prime{num}\n"
-#         for i in range(2,upper):
-#             if num%i == 0:
-#                 result+= f"Not prime{num}\n"
-#         else:
-#             result+= f"Prime{num}\n"
-#     return result
-#
-#
-# rs = prime(3,10)
-# print(rs)
+def prime(lower,upper):
+    result = ""
+    for num in range(lower,upper):
+        if num <2:
+            result+=  f"Not prime{num}\n"
+        for i in range(2,upper):
+            if num%i == 0:
+                result+= f"Not prime{num}\n"
+        else:
+            result+= f"Prime{num}\n"
+    return result
 
-def prime(num):
-        if num>1:
-            for i in range(2,num):
-                if num%i ==0:
-                    print("not prime",num)
-                    break
-            else:
-                print("Prime", num)
 
-for num in range(lower,upper):
-
-#i
+rs = prime(3,10)
+print(rs)
