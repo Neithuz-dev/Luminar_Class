0bb3138549993a719fdb113366723241da3e8fab
Neithuz-dev <neithalprince@gmail.com>
2026-07-22 16:12:39 +0530

    New problems

---
+++ EDA_class/Pandas/customer1.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from pandas import read_csv
+
+df = read_csv(r"C:\Users\hp\Downloads\customer1.txt" ,header= None)
+print(df)
--- EDA_class/Pandas/sample.py
+++ EDA_class/Pandas/sample.py
@@ -5,12 +5,26 @@ df = pd.read_csv(r"C:\Users\hp\Downloads\sample4.txt",sep=',',header = None)
 # print(df.head(5))
 # print(df.tail(4))
 # print(df.ndim)
-df.columns =["id","fname","lname","age","phone_no","location"]
-print(df)
+# df.columns =["id","fname","lname","age","phone_no","location"]
+# print(df)
 
-em_df1= df[["fname","lname","age","phone_no"]]
-print(em_df1)
+# em_df1= df[["fname","lname","age","phone_no"]]
+# print(em_df1)
+#
+# print("-"*100)
+# print((df.describe(include = "all")))
+#
+# df2 = df.iloc[0:6]
+# print(df2)
 
-print("-"*100)
-print((df.describe(include = "all")))
+# df3 = df.iloc[4: ,-1]
+# print(df3)
+#
+# df4 = df.iloc[:,1:4]
+# print(df4)
 
+# df5 = df.iloc[:,[1,2,5]]
+# print(df5)
+
+df6 = df.loc[ :,['fname',"age"]]
+print(df6)
