e4dfe0b397c84ab788298f4a682285e682ec55d1
Neithuz-dev <neithalprince@gmail.com>
2026-07-22 16:31:09 +0530

    New problems

--- EDA_class/Pandas/customer1.py
+++ EDA_class/Pandas/customer1.py
@@ -1,5 +1,15 @@
 import pandas as pd
 from pandas import read_csv
 
-df = read_csv(r"C:\Users\hp\Downloads\customer1.txt" ,header= None)
-print(df)
+df = read_csv(r"C:\Users\hp\Downloads\customer1.txt" ,names= ['id','fname','lname', 'age','prof','loc'])
+# print(df)
+# print(df.shape)
+
+# df1 = df.loc[: ,['fname','age','prof']].head(8)
+# print(df1)
+
+# df2  = df.loc[4:100 ,['fname','lname','prof']]
+# print(df2)
+
+df3 = df.loc[df['loc'] == "india"].head(10)
+print(df3)
