f5ff905ef23a352db508e17e844539852ff3f7c0
Neithuz-dev <neithalprince@gmail.com>
2026-07-23 17:06:24 +0530

    New changes

---
+++ EDA_class/Pandas/groupby_demo.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+df = pd.read_csv(
+    r"C:\Users\hp\Downloads\customer1.txt",
+    names=['id', 'fname', 'lname', 'age', 'prof', 'loc']
+)
+
+# Fill missing location
+df['loc'] = df['loc'].fillna('india')
+# print(df.isna().sum())
+
+# # Location-wise count
+# df1 = df.groupby('loc').count().sort_values(ascending=False)
+# print(df1)
+#
+# # Profession-wise count
+# df2 = df.groupby('prof').count().sort_values(ascending=False)
+# print(df2)
+
+df4 = df.groupby('prof')['age'].max()
+print(df4)
+print('-'*100)
+df5= df.groupby('prof')['age'].min()
+print(df5)
+print('-'*100)
+df6= df.groupby('prof')['age'].mean()
+print(df6)
+
--- EDA_class/Pandas/new_demo.py
+++ EDA_class/Pandas/new_demo.py
@@ -3,7 +3,21 @@ import pandas as pd
 df = pd.read_csv(r"C:\Users\hp\Downloads\customer1.txt" ,names= ['id','fname','lname', 'age','prof','loc'])
 print(df)
 
-print(df.isna().sum())
-df = df.fillna("india")
-print(df.isna().sum())
+# print(df.isna().sum())
+# df = df.fillna("india")
+# print(df.isna().sum())
 
+# df['loc'] = df['loc'].fillna('india')
+# print(df.isna().sum())
+#
+# cs_df = df.drop_duplicates()
+# print(cs_df.shape)
+
+df2 = df.drop(['prof','id'],axis = 1)
+print(df2)
+
+df3 = df.drop([1,2],axis=0)
+print(df3)
+
+df4 = df.count()
+print(df4)
